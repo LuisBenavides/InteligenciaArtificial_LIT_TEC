bcd23b92c811013072516e18a60914749090ca74
LuisBenavides <luisbv@tec.mx>
2022-06-02 19:49:05 -0500

    ejemplo modelos R

--- API-R/plumber.r
+++ API-R/plumber.r
@@ -2,7 +2,15 @@
 #* @param msg The message to echo
 #* @get /echo
 function(msg=""){
-  list(msg = paste0("The message is: '", msg, "'"))
+  # load the model
+  super_model <- readRDS("./final_model.rds")
+  #print(super_model)
+  list(msg = paste0(super_model,": '", msg, "'"))
+  # make a predictions on "new data" using the final model
+  #final_predictions <- predict(super_model, validation[,1:60])
+  #confusionMatrix(final_predictions, validation$Class)
+  
+  #list(msg = paste0("The message is: '", msg, "'"))
 }
 
 #* Plot a histogram
@@ -17,6 +25,12 @@ function(){
 #* @param a The first number to add
 #* @param b The second number to add
 #* @post /sum
-function(a, b){
+function(validation){
   as.numeric(a) + as.numeric(b)
+  # load the model
+  super_model <- readRDS("./final_model.rds")
+  
+  # make a predictions on "new data" using the final model
+  final_predictions <- predict(super_model, validation[,1:60])
+  confusionMatrix(final_predictions, validation$Class)
 }
